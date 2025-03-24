f3d325d531e35137c8412744bfbde50f83130cc2
Krieg <elijahkriegerbusiness@gmail.com>
2025-03-24 00:07:09 -0600

    Update dev_main.zig

--- src/dev_main.zig
+++ src/dev_main.zig
@@ -2,11 +2,14 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 const zs2 = @import("zs2.zig");
+const ee = zs2.ee;
+const gs = zs2.gs;
 
 export fn __start() callconv(.{ .mips_o32 = .{} }) void {
     main();
 }
 
 pub fn main() void {
-
+    gs.BGCOLOR.* = @bitCast(gs.RGB{ .r = 0, .g = 0, .b = 255 });
+    while (true) {}
 }
